7ae48836c9a1a877207c58196edb161a95ceebd9
Ramil Burangulov <ramil.burangulov@helastel.com>
2021-03-28 11:12:21 +0400

    Fix

--- src/producer.py
+++ src/producer.py
@@ -1,6 +1,8 @@
 from kafka import KafkaProducer
+import time
 
 producer = KafkaProducer(bootstrap_servers='kafka1:9092')
 
 while True:
     producer.send('input', 'Hello, World!')
+    time.sleep(5)
