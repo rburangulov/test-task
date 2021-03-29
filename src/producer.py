530935a1e39be5796db03f3a9c2cf535016b8bca
Ramil Burangulov <ingless1991@gmail.com>
2021-03-29 21:18:42 +0400

    Update producer.py

--- src/producer.py
+++ src/producer.py
@@ -1,7 +1,7 @@
 from kafka import KafkaProducer
 import time
 
-producer = KafkaProducer(bootstrap_servers='kafka1:9092')
+producer = KafkaProducer(bootstrap_servers='kafka-service:9092')
 
 while True:
     producer.send('input', 'Hello, World!')
